6adbbec8809e5a64d14166ca1264d750ceae29ac
valentin.martinet <valentin.martinet@>
2025-08-28 16:54:03 +0200

    Fix override script name

--- ci_pre_xcodebuild.sh
+++ ci_pre_xcodebuild.sh
@@ -3,8 +3,9 @@
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH
 
-if [ -f "ios/setup_flutter_xcodecloud.sh" ]; then
+if [ -f "ios/setup_xcodecloud.sh" ]; then
     echo "Found pre-xcodebuild script. Executing."
+    ./setup_xcodecloud.sh
 else
     echo "No pre-xcodebuild script. Skipping."
 fi
